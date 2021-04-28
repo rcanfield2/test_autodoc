ece10e3e82927300231c5896ffbbea492a9e39c4
Ryan Canfield <rcanfiel@uw.edu>
2021-04-28 09:31:58 -0700

    Add more

--- my_project/main.py
+++ my_project/main.py
@@ -12,10 +12,11 @@ def practice_func_ryan(user_name):
         user_name (str): Indicates the name of the person.
     
     Returns:
-        tuple: tuple containing
-            (str): Message from person
+        tuple: tuple containing:
+        
+        (str): Message from person
 
-            (str): Name of person
+        (str): Name of person
     """
     string_out = 'Hello Ryan, from' + user_name
     return string_out, user_name
