c57c99dfc1c8380a82aee23d30aea5d54675e294
Ryan Canfield <rcanfiel@uw.edu>
2021-04-28 08:02:01 -0700

    t

--- my_project/main.py
+++ my_project/main.py
@@ -31,3 +31,39 @@ def practice_func_leo(number_of_fish):
     """
     
     return 'You have {} fish'.format(number_of_fish)
+
+
+"""
+Main Part 2
+====================================
+Repeated module of my example project
+"""
+
+def practice_func_ryan2(user_name):
+    """
+    Return a statement from a person
+
+    Args:
+        user_name (str): Indicates the name of the person.
+
+    Returns: 
+        (tuple): tuple containing:
+            (str): Message from person
+            (str): Name of person
+    """
+    string_out = 'Hello Ryan, from' + user_name
+    return string_out, user_name
+
+
+def practice_func_leo2(number_of_fish):
+    """
+    This function prints a number of fish
+    
+    Args:
+        number_of_fish [int]: How many fish you have
+        
+    Returns:
+        str: a statement of how many fish you have
+    """
+    
+    return 'You have {} fish'.format(number_of_fish)
