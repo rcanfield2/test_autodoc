7798fa7b55a6887d210fb9601a274823d96f1975
Ryan Canfield <rcanfiel@uw.edu>
2021-04-27 23:28:20 -0700

    v0.3

---
+++ my_project/plotting.py
@@ -0,0 +1,19 @@
+"""
+plotting
+====================================
+Plotting module
+"""
+
+def plot_line(x):
+    """
+    This function prints a number of fish
+    
+    Parameters
+    ----------
+    Inputs:
+        x [array]: Data
+        
+    Outputs:
+        
+    """
+    plt.plot(x)
