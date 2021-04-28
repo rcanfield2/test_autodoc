dc32cf9d88d2b3575eb1cb2f028e6e502cf0a492
Ryan Canfield <rcanfiel@uw.edu>
2021-04-28 08:39:47 -0700

    Add more

--- my_project/main.py
+++ my_project/main.py
@@ -12,7 +12,7 @@ def practice_func_ryan(user_name):
         user_name (str): Indicates the name of the person.
     
     Returns:
-        (tuple): tuple containing:
+        tuple: tuple containing:
             (str): Message from person
 
             (str): Name of person
@@ -26,7 +26,7 @@ def practice_func_leo(number_of_fish):
     This function prints a number of fish
     
     Args:
-        number_of_fish [int]: How many fish you have
+        number_of_fish (int): How many fish you have
         
     Returns:
         str: a statement of how many fish you have
